d1a50c71f0287b9281941e8c1e4fa542537a7084
Zyn <bobbytatum999@gmail.com>
2026-08-12 14:57:37 -0500

    Add Filza-native ByeTunes Music Library interface

---
+++ FilzaByeTunesUI.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+FOUNDATION_EXPORT void FilzaByeTunesUIStart(void);
+FOUNDATION_EXPORT void FilzaPresentByeTunesLibrary(UIViewController * _Nullable presenter);
+
+NS_ASSUME_NONNULL_END
