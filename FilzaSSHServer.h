78d176a5fc3e0b61c429fbe5c0490d2c736d30d6
Zyn <bobbytatum999@gmail.com>
2026-08-15 10:40:52 -0500

    Add embedded SSH server interface

---
+++ FilzaSSHServer.h
@@ -0,0 +1,28 @@
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+FOUNDATION_EXPORT NSString * const FilzaSSHEnabledKey;
+FOUNDATION_EXPORT NSString * const FilzaSSHPortKey;
+FOUNDATION_EXPORT NSString * const FilzaSSHBonjourKey;
+FOUNDATION_EXPORT NSString * const FilzaSSHAuthenticationKey;
+FOUNDATION_EXPORT NSString * const FilzaSSHUsernameKey;
+FOUNDATION_EXPORT NSString * const FilzaSSHPasswordSaltKey;
+FOUNDATION_EXPORT NSString * const FilzaSSHPasswordHashKey;
+
+FOUNDATION_EXPORT NSInteger FilzaSSHConfiguredPort(void);
+FOUNDATION_EXPORT BOOL FilzaSSHBonjourEnabled(void);
+FOUNDATION_EXPORT BOOL FilzaSSHAuthenticationEnabled(void);
+FOUNDATION_EXPORT NSString *FilzaSSHConfiguredUsername(void);
+FOUNDATION_EXPORT BOOL FilzaSSHPasswordConfigured(void);
+FOUNDATION_EXPORT BOOL FilzaSSHStorePassword(NSString *password, NSError **error);
+
+FOUNDATION_EXPORT BOOL FilzaSSHServerStart(NSError **error);
+FOUNDATION_EXPORT void FilzaSSHServerStop(void);
+FOUNDATION_EXPORT BOOL FilzaSSHServerRestart(NSError **error);
+FOUNDATION_EXPORT BOOL FilzaSSHServerIsRunning(void);
+FOUNDATION_EXPORT NSString * _Nullable FilzaSSHServerLastError(void);
+FOUNDATION_EXPORT NSString * _Nullable FilzaSSHServerLANAddress(void);
+FOUNDATION_EXPORT NSString *FilzaSSHServerConnectionSummary(void);
+
+NS_ASSUME_NONNULL_END
