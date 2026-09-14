87d2f2005ccd8948be3cb42cf1d620c29c8728a8
Zyn <bobbytatum999@gmail.com>
2026-09-14 17:19:07 -0500

    Compile endpoint admission probe

--- RuntimeStability.m
+++ RuntimeStability.m
@@ -66,3 +66,4 @@ __attribute__((constructor)) static void RSRuntimeStabilityInit(void) {
 // Airlift is compiled into this already-built translation unit so the
 // experiment does not disturb Filza-27's large Theos source graph.
 #import "AirliftOnDeviceProbe.inc"
+#import "AirliftEndpointAdmissionProbe.inc"
