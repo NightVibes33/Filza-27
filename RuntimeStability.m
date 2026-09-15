0337e3ee4d68f0b2f43d0c3b8fa97ee7c4db4e17
Zyn <bobbytatum999@gmail.com>
2026-09-14 20:24:11 -0500

    Wire AirTraffic RSD shim connect probe

--- RuntimeStability.m
+++ RuntimeStability.m
@@ -69,3 +69,4 @@ __attribute__((constructor)) static void RSRuntimeStabilityInit(void) {
 #import "AirliftEndpointAdmissionProbe.inc"
 #import "AirliftLockdownTransportProbe.m"
 #import "AirliftRSDServiceDiscoveryProbe.m"
+#import "AirliftRSDShimConnectProbe.m"
