326f5860c4aa705676a6becf4f3b36506f1711fb
Zyn <bobbytatum999@gmail.com>
2026-09-16 12:03:15 -0500

    airlift: expose Books state helper interface

--- AirliftBooksState.h
+++ AirliftBooksState.h
@@ -1,13 +1,7 @@
 #import <Foundation/Foundation.h>
-
 struct AfcClientHandle;
-
 NS_ASSUME_NONNULL_BEGIN
-
-// Snapshots/restores the AFC-visible Books sync preimage used by the Airlift
-// canary. Snapshot data lives in the app container, never in Media/AFC.
 NSDictionary *FZACSnapshotBooksState(struct AfcClientHandle *afc, NSString *root);
 BOOL FZACBooksStateMatchesSnapshot(struct AfcClientHandle *afc, NSString *root, NSDictionary *snapshot);
 NSDictionary *FZACRestoreBooksState(struct AfcClientHandle *afc, NSString *root);
-
 NS_ASSUME_NONNULL_END
