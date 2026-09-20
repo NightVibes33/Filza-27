e0cb684270312214be44d5080125c8ae6278655f
Zyn <bobbytatum999@gmail.com>
2026-09-19 19:56:51 -0500

    add canonical four-feature navigation router

---
+++ FilzaFeatureRouter.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+FOUNDATION_EXPORT NSString *const FilzaFeature3105;
+FOUNDATION_EXPORT NSString *const FilzaFeatureMusic;
+FOUNDATION_EXPORT NSString *const FilzaFeatureGestalt;
+FOUNDATION_EXPORT NSString *const FilzaFeatureAirCard;
+FOUNDATION_EXPORT NSArray<NSString *> *FilzaCanonicalFeatureIdentifiers(void);
+FOUNDATION_EXPORT BOOL FilzaPresentFeature(NSString *feature, UIViewController * _Nullable source);
+NS_ASSUME_NONNULL_END
