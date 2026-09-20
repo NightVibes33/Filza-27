0fb021522982744c9a65e49d93c3461f20426665
Zyn <bobbytatum999@gmail.com>
2026-09-19 19:56:54 -0500

    add canonical four-feature navigation router implementation

---
+++ FilzaFeatureRouter.m
@@ -0,0 +1,30 @@
+@import UIKit;
+#import "FilzaFeatureRouter.h"
+#import "Filza3105Bridge.h"
+#import "ByeTunesFullAppLauncher.h"
+#import "FilzaMondBridge.h"
+#import "FilzaAirCardBridge.h"
+#import "FilzaDiagnostics.h"
+
+NSString *const FilzaFeature3105 = @"com.nightvibes33.filzaslop.3105";
+NSString *const FilzaFeatureMusic = @"com.nightvibes33.filzaslop.music-library";
+NSString *const FilzaFeatureGestalt = @"com.nightvibes33.filzaslop.gestalt-manager";
+NSString *const FilzaFeatureAirCard = @"com.nightvibes33.filzaslop.aircard";
+
+NSArray<NSString *> *FilzaCanonicalFeatureIdentifiers(void) {
+    return @[FilzaFeature3105, FilzaFeatureMusic, FilzaFeatureGestalt, FilzaFeatureAirCard];
+}
+
+BOOL FilzaPresentFeature(NSString *feature, UIViewController *source) {
+    BOOL opened = NO;
+    if ([feature isEqualToString:FilzaFeature3105])
+        opened = Filza3105PresentHomeFromController(source);
+    else if ([feature isEqualToString:FilzaFeatureMusic])
+        opened = FilzaByeTunesPresentFromController(source);
+    else if ([feature isEqualToString:FilzaFeatureGestalt]) {
+        if (source) { FilzaMondPresentFromController(source); opened = YES; }
+    } else if ([feature isEqualToString:FilzaFeatureAirCard])
+        opened = FilzaAirCardPresentFromController(source);
+    FilzaDiagnosticsAppend(@"FeatureRouter", [NSString stringWithFormat:@"%@ %@", opened ? @"opened" : @"failed", feature ?: @"nil"]);
+    return opened;
+}
