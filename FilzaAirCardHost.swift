14c92866a70b468265a71d530d1900d81e73130c
Zyn <bobbytatum999@gmail.com>
2026-09-20 00:10:40 -0500

    present AirCard in shared embedded app panel

--- FilzaAirCardHost.swift
+++ FilzaAirCardHost.swift
@@ -19,12 +19,18 @@ private enum AirCardEmbeddedRuntime {
 private func ALGetGrappaToken(_ a: UInt32,_ b: UInt32,_ c: UInt32,_ d: UnsafeMutablePointer<UInt8>?,_ e: Int,_ f: UnsafeMutablePointer<Int>?,_ g: UnsafeMutablePointer<CChar>?,_ h: Int)->Int32
 private struct AirCardEmbeddedRoot: View {
     @StateObject private var vm = AppViewModel()
-    var body: some View { AirCardContentView().environmentObject(vm).task { _ = await LocalNetworkAuthorization().request(timeout: 2.5) } }
+    var body: some View {
+        FilzaEmbeddedPanel {
+            AirCardContentView()
+                .environmentObject(vm)
+                .task { _ = await LocalNetworkAuthorization().request(timeout: 2.5) }
+        }
+    }
 }
 @MainActor @objc(AirCardEmbeddedHostFactory)
 public final class AirCardEmbeddedHostFactory: NSObject {
     @objc public static func makeViewController() -> UIViewController {
         AirCardEmbeddedRuntime.configureOnce()
-        let c=UIHostingController(rootView: AirCardEmbeddedRoot()); c.modalPresentationStyle = .fullScreen; c.title="AirCard"; return c
+        let c = UIHostingController(rootView: AirCardEmbeddedRoot()); c.title = "AirCard"; FilzaEmbeddedPanelPresentation.configure(c); return c
     }
 }
